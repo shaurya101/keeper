2fce3d7804faea84e9c96f5605406a358c8df91b
shaurya101 <shauryabhandari191@gmail.com>
2023-05-05 16:33:28 +0530

    Add note functionality implemented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
 import CreateArea from "./CreateArea";
 
 function App() {
+    const[notes, setNotes] = useState([]); // initialize empty array which storesa all notes
+    function addNote(newNote) {
+        setNotes(prevNotes=> {
+            return [...prevNotes, newNote];
+        })
+    }
+
     return (
         <div>
             <Header />
-            <CreateArea />
-            <Note key={1} title="Note title" content="Note content" />
+            <CreateArea onAdd={addNote}/>
+            {notes.map(noteItem => {
+                return <Note key={1} title={noteItem.title} content={noteItem.content} /> 
+            })}
             <Footer />
         </div>
     );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+    const [ note, setNote ] = useState({
+        title: "",
+        content: ""
+    });
+    function handleChange(event) {
+        const { name, value } = event.target;   // destructuring event
+
+        setNote((prevNote)=>{
+            return {...prevNote, [name]: value};  // enclose name in [] else it will read it as a String and not the destrucctured variable!
+        })
+    }
+    function submitNote(event) {
+        props.onAdd(note);       // to call onAdd prop which calls addNote() in App.js
+        event.preventDefault();  // to prevent refreshing of page everytime when we submit node
+        setNote({                // to resent textArea for next Note to be written
+            title: "",
+            content: ""
+        })
+    }
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+      <form onSubmit={submitNote}>
+        <input name="title" placeholder="Title" onChange={handleChange} value={note.title} style={{fontWeight : "bold"}} />
+        <textarea name="content" placeholder="Take a note..." rows="3" onChange={handleChange} value={note.content} />
         <button>Add</button>
       </form>
     </div>
