32d7e18e47325386ae99b15da1e5a77f19412b6e
shaurya101 <shauryabhandari191@gmail.com>
2023-05-05 14:12:06 +0530

    Initial layout before functionality

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+import CreateArea from "./CreateArea";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <CreateArea />
+            <Note key={1} title="Note title" content="Note content" />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-function Note() {
-    return (
-        <div className="note">
-            <h1>Title</h1>
-            <p>Body</p>
-        </div>
-    );
+function Note(props) {
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button>DELETE</button>
+    </div>
+  );
 }
 
 export default Note;
